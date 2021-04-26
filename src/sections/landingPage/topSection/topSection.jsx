f1b9d7472d6e6f8d02edfa2f363a791b2ff915f6
Danie Don <dandons25@gmail.com>
2021-04-26 18:13:32 +0100

    add small fixes and updates

--- src/sections/landingPage/topSection/topSection.jsx
+++ src/sections/landingPage/topSection/topSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Row, Col } from 'antd'
+import Typewriter from 'typewriter-effect'
 import './styles.scss'
 import GeneralButton from '../../../components/GeneralButton'
 
@@ -7,7 +8,7 @@ const TopSection = () => {
   return (
     <div className="entire-top-section">
       <Row className="flex-container">
-        <Col sm={24} md={9} className="flex-1">
+        <Col sm={24} lg={9} className="flex-1">
           <div className="image-container">
           </div>
         </Col>
@@ -15,9 +16,15 @@ const TopSection = () => {
           <h1>
             Kickstart your <br/> web development <br/> career
           </h1>
-          <p>
-            Transition from any career and join others in <br/> learning frontend web development from <br/> scratch
-          </p>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter.typeString("<p>Transition from any career and join others in <br/> learning frontend web development from <br/> scratch</p>").start()
+            }}
+            options = {{
+              delay: 0.2,
+              cursor: " "
+            }}
+          />
           <div className="large-screen-button">
             <GeneralButton
               buttonText="Signup for free"
