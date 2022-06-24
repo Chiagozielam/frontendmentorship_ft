87dea5d64461c0dc100781d5ca3bdcc55b39f821
Addresser001 <sokarithankgod2@gmail.com>
2022-06-23 20:28:40 -1100

    fixing

--- src/pages/newPassword/newPassword.jsx
+++ src/pages/newPassword/newPassword.jsx
@@ -1,21 +1,19 @@
-
-
-gitimport "./styles.scss";
-import { Row, Col, Alert, Spin } from "antd";
-import NormalInput from "../../components/form/normalInput/normalInput";
-import GeneralButton from "../../components/GeneralButton";
-import { useState } from "react";
-import { BASE_URI } from "../../constants/baseUri";
-import axios from "axios";
-import { REGISTRATION_PAGE } from "../../routes";
-import { EyeOutlined } from "@ant-design/icons";
-import HeaderText from "../../components/headerText/headerText";
-import NormalText from "../../components/normalText/normalText";
-import { Link, useHistory } from "react-router-dom";
+import './styles.scss';
+import { Row, Col, Alert, Spin } from 'antd';
+import NormalInput from '../../components/form/normalInput/normalInput';
+import GeneralButton from '../../components/GeneralButton';
+import { useState } from 'react';
+import { BASE_URI } from '../../constants/baseUri';
+import axios from 'axios';
+import { REGISTRATION_PAGE } from '../../routes';
+import { EyeOutlined } from '@ant-design/icons';
+import HeaderText from '../../components/headerText/headerText';
+import NormalText from '../../components/normalText/normalText';
+import { Link, useHistory } from 'react-router-dom';
 
 const SetPassword = () => {
   const token = new URLSearchParams(document.location.search.substring(1)).get(
-    "token"
+    'token'
   );
 
   const { push } = useHistory();
@@ -25,12 +23,12 @@ const SetPassword = () => {
   // }
 
   const [showAlert, setShowAlert] = useState(false);
-  const [alertMessage, setAlertMessage] = useState("");
+  const [alertMessage, setAlertMessage] = useState('');
 
-  const [newPassword, setNewPassword] = useState("");
-  const [confrimPassword, setConfrimPassword] = useState("");
+  const [newPassword, setNewPassword] = useState('');
+  const [confrimPassword, setConfrimPassword] = useState('');
   const [showErrorMessage, setShowErrorMessage] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const sendNewPassword = async (e) => {
@@ -41,10 +39,10 @@ const SetPassword = () => {
       let submitObject = { newPassword };
 
       let config = {
-        method: "post",
+        method: 'post',
         url: `${BASE_URI}/user/changepassword`,
         headers: {
-          "user-token": token,
+          'user-token': token,
         },
         data: submitObject,
       };
@@ -55,81 +53,81 @@ const SetPassword = () => {
         setShowAlert(true);
         setIsLoading(false);
         setTimeout(() => {
-          push("/login");
+          push('/login');
         }, 3500);
       } catch (err) {
         console.log(err);
       }
     } else if (newPassword !== confrimPassword) {
       setShowErrorMessage(true);
-      setErrorMessage("Passwords do not match");
+      setErrorMessage('Passwords do not match');
       setTimeout(() => {
         setShowErrorMessage(false);
       }, 3000);
     } else if (newPassword.length < 6) {
       setShowErrorMessage(true);
-      setErrorMessage("Password must be up to 6 characters");
+      setErrorMessage('Password must be up to 6 characters');
       setTimeout(() => {
         setShowErrorMessage(false);
       }, 3000);
     }
   };
   return (
-    <div className="new-password">
+    <div className='new-password'>
       <Row>
         <Col sm={24} md={11}>
-          <div className="image-container">
-            <div className="sub_container">
-              <div className="text_container">
-                <HeaderText text="Welcome!" className="welcome_header" />
+          <div className='image-container'>
+            <div className='sub_container'>
+              <div className='text_container'>
+                <HeaderText text='Welcome!' className='welcome_header' />
                 <NormalText
-                  className="sub_text"
-                  text="Mi libero neque consectetur amet mi neque cursus. Dignissim facilisi vitae consequat facilisis bibendum ultrices."
+                  className='sub_text'
+                  text='Mi libero neque consectetur amet mi neque cursus. Dignissim facilisi vitae consequat facilisis bibendum ultrices.'
                 />
               </div>
             </div>
           </div>
         </Col>
         <Col sm={24} md={13}>
-          <div className="form-container">
+          <div className='form-container'>
             {showAlert && (
-              <div className="form-alert">
+              <div className='form-alert'>
                 <Alert
                   message={alertMessage}
-                  type="success"
+                  type='success'
                   closable
                   onClose={() => setShowAlert(false)}
                 />
               </div>
             )}
-            <div className="form_sub_container">
-              <HeaderText text="Create New Password" className="form_header" />
+            <div className='form_sub_container'>
+              <HeaderText text='Create New Password' className='form_header' />
               <form onSubmit={sendNewPassword}>
-                <div className="each_input_fild">
-                  <div className="input_field_container">
-                    <EyeOutlined className="icon" />
+                <div className='each_input_fild'>
+                  <div className='input_field_container'>
+                    <EyeOutlined className='icon' />
                     <NormalInput
-                      label="New Password"
-                      placeholder="Input new Password"
-                      type="password"
+                      label='New Password'
+                      placeholder='Input new Password'
+                      type='password'
                       inputValue={newPassword}
                       onChange={(event) => setNewPassword(event.target.value)}
                     />
                   </div>
                   {showErrorMessage && (
-                    <p style={{ color: "red", textAlign: "left" }}>
+                    <p style={{ color: 'red', textAlign: 'left' }}>
                       {errorMessage}
                     </p>
                   )}
                 </div>
 
-                <div className="each_input_fild">
-                  <div className="input_field_container">
-                    <EyeOutlined className="icon" />
+                <div className='each_input_fild'>
+                  <div className='input_field_container'>
+                    <EyeOutlined className='icon' />
                     <NormalInput
-                      label="Confirm Password"
-                      type="password"
-                      placeholder="Confirm password"
+                      label='Confirm Password'
+                      type='password'
+                      placeholder='Confirm password'
                       inputValue={confrimPassword}
                       onChange={(event) =>
                         setConfrimPassword(event.target.value)
@@ -137,37 +135,37 @@ const SetPassword = () => {
                     />
                   </div>
                   {showErrorMessage && (
-                    <p style={{ color: "red", textAlign: "left" }}>
+                    <p style={{ color: 'red', textAlign: 'left' }}>
                       {errorMessage}
                     </p>
                   )}
                 </div>
 
-                <div className="submit-btn">
+                <div className='submit-btn'>
                   {isLoading ? (
                     <Spin
-                      tip="Creating new password..."
+                      tip='Creating new password...'
                       style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        marginTop: "-5%",
-                        marginBottom: "10%",
-                        padding: "5%",
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        marginTop: '-5%',
+                        marginBottom: '10%',
+                        padding: '5%',
                       }}
                     />
                   ) : (
                     <GeneralButton
-                      buttonText="Submit"
-                      width="100%"
-                      height="54px"
-                      borderRadius="10px"
-                      htmlType="submit"
+                      buttonText='Submit'
+                      width='100%'
+                      height='54px'
+                      borderRadius='10px'
+                      htmlType='submit'
                     />
                   )}
                 </div>
-                <p className="create-account">
-                  Don&rsquo;t have an account yet?{" "}
+                <p className='create-account'>
+                  Don&rsquo;t have an account yet?{' '}
                   <Link to={REGISTRATION_PAGE}>Create a free acount here</Link>
                 </p>
               </form>
